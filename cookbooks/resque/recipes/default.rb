b292ce029cd714fce87531ff10aeaa02cd83cec8
Dirk Holzapfel <dirk@bitcrowd.net>
2011-10-07 11:36:14 +0200

    new resque tries

--- cookbooks/resque/recipes/default.rb
+++ cookbooks/resque/recipes/default.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: resque
 # Recipe:: default
 #
-if ['solo', 'app', 'app_master', 'util'].include?(node[:instance_role])
+if ['db_host'].include?(node[:instance_role])
   
   execute "install resque gem" do
     command "gem install resque redis redis-namespace yajl-ruby -r"
@@ -18,25 +18,35 @@ if ['solo', 'app', 'app_master', 'util'].include?(node[:instance_role])
     end
   
 
+    template "/data/#{app}/shared/config/resque.yml" do
+      owner node[:owner_name]
+      group node[:owner_name]
+      mode 0644
+      source "resque_yaml.conf.erb"
+      variables({        
+        :db_host => node["db_host"]
+      })
+    end
+    
     node[:applications].each do |app, data|
       template "/etc/monit.d/resque_#{app}.monitrc" do 
-      owner 'root' 
-      group 'root' 
-      mode 0644 
-      source "monitrc.conf.erb" 
-      variables({ 
-      :num_workers => worker_count,
-      :app_name => app, 
-      :rails_env => node[:environment][:framework_env] 
-      }) 
+        owner 'root' 
+        group 'root' 
+        mode 0644 
+        source "monitrc.conf.erb" 
+        variables({ 
+          :num_workers => worker_count,
+          :app_name => app, 
+          :rails_env => node[:environment][:framework_env] 
+        }) 
       end
 
       worker_count.times do |count|
         template "/data/#{app}/shared/config/resque_#{count}.conf" do
-        owner node[:owner_name]
-        group node[:owner_name]
-        mode 0644
-        source "resque_wildcard.conf.erb"
+          owner node[:owner_name]
+          group node[:owner_name]
+          mode 0644
+          source "resque_wildcard.conf.erb"
         end
       end
 
